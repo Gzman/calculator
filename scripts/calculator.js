32195041d4dfd47ca521fab2218d1d1d810ea989
Gzman <ganzmann@protonmail.com>
2021-04-17 14:13:26 +0200

    Added script with all math operations

---
+++ scripts/calculator.js
@@ -0,0 +1,13 @@
+
+export const addition = (a, b) => a + b;
+
+export const subtraction = (a, b) => a - b;
+
+export const multiplication = (a, b) => a * b;
+
+export const division = (a, b) => a / b;
+
+export const square = (a) => a**2;
+
+export const radix = (a) => Math.radix(a);
+
