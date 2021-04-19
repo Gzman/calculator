c58009f646f6d90c45a9388b196e784e895c35cd
Gzman <ganzmann@protonmail.com>
2021-04-20 01:36:52 +0200

    Simplified/refactored code

--- scripts/index.js
+++ scripts/index.js
@@ -1,89 +1,73 @@
 import { operate, getOperatorByName } from "./calculator.js"
 
-const FIRST = 0;
-const SECOND = 1;
-const OPERATOR = 2;
-
-let currentIndex = 0;
-let inputs = ["", "", ""];
-let result = "";
-
 const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number");
 const clearButton = document.querySelector(".clear");
-const operatorButtons = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector("#equals");
 
-const resetInput = () => {
-    inputs = ["", "", ""];
-    currentIndex = 0;
-};
-const resetResult = () => result = "";
-const clearDisplay = () => display.value = 0;
-const displayNumber = (number) => display.value = number;
-const isFirstNumberSet = () => inputs[FIRST].length > 0;
-const isSecondNumberSet = () => inputs[SECOND].length > 0;
-const isOperatorSet = () => inputs[OPERATOR].length > 0;
-const isInputComplete = () => isFirstNumberSet() && isSecondNumberSet() && isOperatorSet();
-const isInputEvalueted = () => result.length > 0 && isFirstNumberSet() && isSecondNumberSet();
+clearButton.addEventListener("click", clearClicked);
+equalsButton.addEventListener("click", equalsClicked);
+document.querySelectorAll(".number").forEach(button => button.addEventListener("click", numberClicked));
+document.querySelectorAll(".operator").forEach(button => button.addEventListener("click", operationClicked));
 
-function calculate() {
-    const first = parseInt(inputs[FIRST]);
-    const second = parseInt(inputs[SECOND]);
-    const operator = getOperatorByName(inputs[OPERATOR]);
-    return operate(first, second, operator);
+let firstNumber = display.value;
+let secondNumber = "";
+let operator = "";
+let isEvaluationDisplayed = false;
+
+const reset = () => {
+    display.value = "0";
+    firstNumber = display.value;
+    secondNumber = "";
+    operator = "";
+};
+
+const calculate = () => operate(parseInt(firstNumber), parseInt(secondNumber), getOperatorByName(operator));
+
+// const isUnaryOperator = (operator) => operator === "square" || operator === "radix";
+
+const roundNumber = number => Math.round(number * 100) / 100;
+
+function evaluate() {
+    if (firstNumber !== "" && secondNumber !== "" && operator !== "") {
+        let result = roundNumber(calculate());
+        console.log("result", result);
+        if (result === Infinity) {
+            alert("You can't divide by zero!");
+            result = 0;
+        }
+        reset();
+        firstNumber = result;
+        display.value = firstNumber;
+        isEvaluationDisplayed = true;
+    }
 }
 
 function numberClicked(event) {
-    if (isOperatorSet()) {
-        displayNumber("");
-    }
-    if (isInputEvalueted()) {
-        resetInput();
-        resetResult();
-        currentIndex = FIRST;
-    }
     const number = event.target.textContent;
-    inputs[currentIndex] += number;
-    displayNumber(inputs[currentIndex]);
-}
-
-const evaluateInput = () => {
-    resetResult();
-    result += calculate();
-    displayNumber(result);
+    if (operator === "") {
+        if (firstNumber === "0" || isEvaluationDisplayed) {
+            firstNumber = "";
+            isEvaluationDisplayed = false;
+        }
+        firstNumber += number;
+        display.value = firstNumber;
+    }
+    else {
+        secondNumber += number;
+        display.value = secondNumber;
+    }
 }
 
 function operationClicked(event) {
-    console.log(this.name, inputs);
-    if (isFirstNumberSet()) {
-        currentIndex = SECOND;
-    }
-    if (isInputComplete()) {
-        evaluateInput();
-    }
-    if (isInputEvalueted()) {
-        resetInput();
-        inputs[FIRST] += result;
-        resetResult();
-        currentIndex = SECOND;
-    }
-    const operator = event.target.id;
-    inputs[OPERATOR] = operator;
+    operator = event.target.id;
+    console.log(operator);
+    evaluate();
 }
 
 function clearClicked() {
-    clearDisplay();
-    resetInput();
+    reset();
 }
 
 function equalsClicked() {
-    if (isInputComplete()) {
-        evaluateInput();
-    }
+    evaluate();
 }
-
-numberButtons.forEach(button => button.addEventListener("click", numberClicked));
-operatorButtons.forEach(button => button.addEventListener("click", operationClicked));
-clearButton.addEventListener("click", clearClicked);
-equalsButton.addEventListener("click", equalsClicked);
